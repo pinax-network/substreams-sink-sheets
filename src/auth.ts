02ef49b47ccb53f98a187e33012077de2e7c7aef
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-03-02 12:53:06 -0500

    Comment out `fs` related operation for testing client-side only

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+//import fs from 'fs'
 import { google } from 'googleapis'
 
 export interface Credentials {
@@ -14,7 +14,7 @@ export interface ServiceCredentials {
 
 export async function authenticateOAuth2(credentials: Credentials) {
     const auth = new google.auth.OAuth2()
-    auth.setCredentials(credentials);
+    auth.setCredentials(credentials)
     return google.sheets({ version: 'v4', auth})
 }
 
@@ -69,12 +69,13 @@ export async function parseServiceCredentials(json_file_url: string): Promise<Se
         return await response.json()
     // handle as file
     } else {
-        if ( fs.existsSync(json_file_url) === false ) throw new Error(`Credentials file not found: ${json_file_url}`)
+        throw new Error('No filesystem here !')
+        /*if ( fs.existsSync(json_file_url) === false ) throw new Error(`Credentials file not found: ${json_file_url}`)
 
         try {
             return JSON.parse(fs.readFileSync(json_file_url, {encoding: 'utf-8'}))
         } catch (e) {
             throw new Error(`Error parsing credentials file: ${json_file_url}`)
-        }
+        }*/
     }
 }
